cbb7eae0c9052bb29dffa1193517221880d580ce
unknown <FrosinaG>
2023-02-27 20:32:53 +0100

    merge

--- src/Component/AllProject.js
+++ src/Component/AllProject.js
@@ -18,6 +18,8 @@ import fresh from "../Portfolio-img/react-fresh.png";
 import newreact from "../Portfolio-img/react-new.png";
 import projectreact from "../Portfolio-img/react-project.png";
 import test from "../Portfolio-img/react-test.png";
+import shopreact from "../Portfolio-img/react-shop.png"
+import Reactproject from "./Reactproject";
 
 
 const AllProject = () => {
@@ -342,6 +344,26 @@ const AllProject = () => {
             </p>
           </a>
         </div>{" "}
+        <div className="container-img">
+          <a href="https://f-online-store.netlify.app/">
+            <img src={shopreact } alt="project"className="all-portfolio" />{" "}
+            <p className="view">
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                className="bi bi-link"
+                viewBox="0 0 16 16"
+              >
+                <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z" />
+                <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z" />
+              </svg>
+              view more
+            </p>
+          </a>
+        </div>
         <div className="container-img">
           <a href="https://github.com/FrosinaG/project-new">
             <img src={newreact} alt="project" className="all-portfolio" />{" "}
